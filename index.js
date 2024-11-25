382813953b0f8774243bccc11be1e0ae0c06b984
duchieu1999 <46242272+duchieu1999@users.noreply.github.com>
2024-11-25 12:57:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2145,7 +2145,7 @@ const Attendance = mongoose.model('Attendance', attendanceSchema);
 const timeSlots = [
   { time: '9:30', label: 'ca 9h30' },
   { time: '11:30', label: 'ca 11h30' },
-  { time: '14:30', label: 'ca 14h30' }, 
+  { time: '13:00', label: 'ca 14h30' }, 
   { time: '18:00', label: 'ca 18h00' },
   { time: '19:30', label: 'ca 19h30' }
 ];
