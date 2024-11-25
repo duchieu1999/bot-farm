f39d21fdc75695b930bf9d7f130a0326ab9ac3fe
duchieu1999 <46242272+duchieu1999@users.noreply.github.com>
2024-11-25 11:01:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2144,13 +2144,13 @@ const Attendance = mongoose.model('Attendance', attendanceSchema);
 
 const timeSlots = [
   { time: '9:30', label: 'ca 9h30' },
-  { time: '11:30', label: 'ca 11h30' },
+  { time: '11:04', label: 'ca 11h30' },
   { time: '14:30', label: 'ca 14h30' }, 
   { time: '18:00', label: 'ca 18h00' },
   { time: '19:30', label: 'ca 19h30' }
 ];
 
-const groupId = -1002280909865;
+const groupId = -1002333438294;
 const adminIds = [7305842707];
 
 let billImagesCount = 0;
