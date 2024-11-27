303d62dc5f75c2c4a22a5984bfed6ab70ae7bc00
duchieu1999 <46242272+duchieu1999@users.noreply.github.com>
2024-11-27 18:05:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2027,11 +2027,11 @@ const timeSlots = [
   { time: '9:30', label: 'ca 10h00' },
   { time: '11:30', label: 'ca 12h00' },
   { time: '14:30', label: 'ca 15h00' }, 
-  { time: '18:00', label: 'ca 18h30' },
+  { time: '18:08', label: 'ca 18h30' },
   { time: '19:30', label: 'ca 20h00' }
 ];
 
-const groupId = -1002333438294;
+const groupId = -1002280909865;
 const adminIds = [7305842707];
 const topicId = 10;
 
