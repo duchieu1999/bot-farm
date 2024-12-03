dd566ac3882653c397ae14990379fc95a2be45c2
duchieu1999 <46242272+duchieu1999@users.noreply.github.com>
2024-12-03 10:47:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -186,7 +186,21 @@ function loadFiles() {
 loadFiles();
 
 
-
+// Đảm bảo xóa webhook trước khi polling
+(async () => {
+  try {
+    const webhookInfo = await bot.getWebhookInfo();
+    if (webhookInfo.url) {
+      console.log(`Webhook hiện tại: ${webhookInfo.url}. Đang xóa webhook...`);
+      await bot.deleteWebhook();
+      console.log('Webhook đã được xóa.');
+    } else {
+      console.log('Không có webhook nào đang hoạt động.');
+    }
+  } catch (error) {
+    console.error('Lỗi khi kiểm tra hoặc xóa webhook:', error);
+  }
+})();
 
 // Chuỗi cấmm
 const bannedStringsRegex = /(ca\s?1|ca1|ca\s?2|Ca\s?2|Ca\s?1|Ca1|Ca\s?2|Ca2|C1|C2|c\s?1|c\s?2|C\s?1|C\s?2)\s*/gi;
