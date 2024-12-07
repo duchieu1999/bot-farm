cae1ea9a901a344663298cc8474f3d53f4ad6375
duchieu1999 <46242272+duchieu1999@users.noreply.github.com>
2024-12-07 11:49:35 +0700

    Update ha.js

--- commands/ha.js
+++ commands/ha.js
@@ -26,6 +26,7 @@ module.exports = (bot) => {
             "-1002247863313": "NHÓM HÀ QUẨY ZALO BIN:",
             "-1002303292016": "NHÓM HÀ 11H30, 19H30:",
             "-1002499533124": "NHÓM HÀ PANLEN:"
+            "-1002322022623": "NHÓM HÀ:"
         };
 
         // Kiểm tra nếu groupId tồn tại trong danh sách
